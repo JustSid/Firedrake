ed07e5feb44b8aec3ad12b354046f198761f676a
Sidney Just <Sidney.just@gmail.com>
2013-03-01 03:38:32 +0100

    Build system clean up

--- executeqemu.sh
+++ executeqemu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
+QEMU_NET="-net nic,model=rtl8139 -net user"
 
 if [ $# -eq 0 ]; then
-    qemu -net nic,model=rtl8139 -net user "$BASEDIR/boot/Firedrake.iso"
-    exit 0
+	qemu-system-i386 $QEMU_NET "$BASEDIR/boot/Firedrake.iso"
+	exit 0
 fi
 
 if [ "$@" == "--debug" ]; then
-	qemu -d int,cpu_reset -net nic,model=rtl8139 -net user -no-reboot -no-shutdown "$BASEDIR/boot/Firedrake.iso"
+	qemu-system-i386 -d int,cpu_reset -no-reboot -no-shutdown $QEMU_NET "$BASEDIR/boot/Firedrake.iso"
 	exit 0
 fi
