691a3d2ba851f156d4d95bf198669bcac7feb5ee
Sidney Just <Sidney.just@gmail.com>
2013-12-06 03:12:51 +0100

    Added intmax_t and uintmax_t typedefs to stdint.h

--- sys/libc/stdint.h
+++ sys/libc/stdint.h
@@ -29,6 +29,9 @@ typedef unsigned short      uint16_t;
 typedef unsigned int        uint32_t;
 typedef unsigned long long  uint64_t;
 
+typedef int64_t  intmax_t;
+typedef uint64_t uintmax_t;
+
 typedef int32_t  intptr_t;
 typedef uint32_t uintptr_t;
 
